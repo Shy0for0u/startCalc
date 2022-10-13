e8c748168c5ac4e9f6788db4e5b7fcfb8737b19b
Shy0for0u <pav.tro2014@yandex.ru>
2022-10-14 01:08:22 +0300

    add calc

---
+++ testFunc.ps1
@@ -0,0 +1,13 @@
+﻿#$_b = "dGVzdF9maWxl"
+#[byte[]]$_0=[System.Convert]::FromBase64String($_b):$_1=[System.Threading.Thread]::GetDomain().Load($_0)
+
+
+function parse([string]$s){
+   $H = @()
+   for ($i = 0; $i -lt $s.Length; $i += 2) {
+      $H += [Byte]::Parse($s.Substring($i, 2), [System.Globalization.NumberStyles]::HexNumber);
+   };
+   return $H;
+}
+
+parse("E0")
